99e726459c12cba275f9ca59d73faa1eaa17dfcc
GJSBRT <g@gijs.eu>
2021-03-24 22:42:18 +0100

    Updated repoURL
    Updated repoURL

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/GJSBRT/GJSBRT.github.io.git";
+const repoURL = "https://github.com/GJSBRT/website.git";
 
 ghpages.publish(
   pathname,
